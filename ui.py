6f3ea6e6966a3c71b1d70c99d5e67881b6bf1b7e
unknown <mcdonaldai@etsu.edu>
2024-07-24 19:57:49 -0400

    s with Initial Commit

--- ui.py
+++ ui.py
@@ -53,8 +53,10 @@ class MapNavigator:
         nearest_node, node_dist = ox.distance.nearest_nodes(
             G=self.G, X=position[1], Y=position[0], return_dist=True
         )
+        # Get nearest node coordinates
         nn_x = self.G.nodes[nearest_node]["x"]
         nn_y = self.G.nodes[nearest_node]["y"]
+        # Create a LineString from position to nearest node
         line = LineString([(position[1], position[0]), (nn_x, nn_y)])
         x, y = line.xy
         self.nn_line.set_data(x, y)
@@ -63,20 +65,26 @@ class MapNavigator:
         print(f"Node Distance: {round(node_dist, 6)} meters")
 
     def update_nearest_edge(self, position):
+        # Find nearest edge and its geometry
         nearest_edge, edge_dist = ox.distance.nearest_edges(
             G=self.G, X=position[1], Y=position[0], return_dist=True
         )
+        # Get nearest node coordinates
         if "geometry" in self.G.edges[nearest_edge]:
+            # Get the geometry LineString
             edge_geo = self.G.edges[nearest_edge]["geometry"]
+            # Find the closest point on the edge to the cursor
             point = Point(position[1], position[0])
             edge_point = edge_geo.interpolate(edge_geo.project(point))
             self.prev_edge_point = edge_point
+            # Plot line from cursor to nearest point on edge
             line = LineString(
                 [(position[1], position[0]), (edge_point.x, edge_point.y)]
             )
             x, y = line.xy
             self.ne_line.set_data(x, y)
             self.ax.draw_artist(self.ne_line)
+            # Convert from degrees to meters
             edge_dist = geodesic(
                 (position[0], position[1]), (edge_point.y, edge_point.x)
             ).meters
@@ -90,6 +98,7 @@ class MapNavigator:
             x, y = line.xy
             self.ne_line.set_data(x, y)
             self.ax.draw_artist(self.ne_line)
+            # Convert from degrees to meters
             edge_dist = geodesic(
                 (position[0], position[1]),
                 (self.prev_edge_point.y, self.prev_edge_point.x),
