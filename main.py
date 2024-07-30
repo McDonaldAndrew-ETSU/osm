2651d11446693fecfe30e1b7385aaf7810b89eb6
unknown <mcdonaldai@etsu.edu>
2024-07-29 20:01:21 -0400

    Updates main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ui import MapNavigator
 import matplotlib.pyplot as plt
 from sensors import HeadingController, update_position
+from device import DeviceController
 
 initial_position = (36.3714, -82.2582)  # (lat, long)
 map_navigator = MapNavigator(
@@ -10,12 +11,16 @@ map_navigator = MapNavigator(
 heading_controller = HeadingController()
 map_navigator.fig.canvas.mpl_connect("key_press_event", heading_controller.on_key)
 
+device_controller = DeviceController(navigator=map_navigator)
+map_navigator.fig.canvas.mpl_connect(
+    "key_press_event", device_controller.toggle_map_matching
+)
+
 while True:
     map_navigator.fig.canvas.restore_region(map_navigator.background)
     map_navigator.position = update_position(
         map_navigator.position, heading_controller.heading
     )
     map_navigator.update_cursor(map_navigator.position)
-    map_navigator.update_nearest_edge(map_navigator.position)
-    map_navigator.update_nearest_node(map_navigator.position)
+
     plt.pause(1)
